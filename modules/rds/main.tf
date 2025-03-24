179907a43249f4bcaccccabecca786f651129955
briana026 <briana.rice23@gmail.com>
2025-03-24 00:11:49 -0500

    Omit RDS Engine Version

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -35,7 +35,6 @@ resource "aws_db_instance" "this" {
   identifier              = "${var.environment_name}-mysql-db"
   allocated_storage       = 20
   engine                  = "mysql"
-  engine_version          = "8.0"
   instance_class          = "db.t2.micro"
   db_name                 = var.db_name
   username                = var.db_username
